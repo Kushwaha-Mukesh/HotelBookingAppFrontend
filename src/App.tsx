e23455c16c672ae4fea0edadc313dd7cf6e54909
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-02-24 11:52:29 +0545

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout></Layout>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
