d1ca87a7f21fe7145184e679d6b75aae43f7d9f3
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-02-27 13:06:38 +0545

    added register UI and fetched register endpoint from frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -18,6 +19,15 @@ const App = () => {
           element={
             <Layout>
               <p>Search Page</p>
+            </Layout>
+          }
+        />
+
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
             </Layout>
           }
         />
