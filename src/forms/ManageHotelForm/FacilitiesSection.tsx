fc0c6a45a24284588b84f75e70edd81748d445b2
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-03-06 20:12:54 +0545

    added UI of add-hotel feature

---
+++ src/forms/ManageHotelForm/FacilitiesSection.tsx
@@ -0,0 +1,42 @@
+import { useFormContext } from "react-hook-form";
+import { HotelFormData } from "./ManageHotelForm";
+import { hotelFaci1ities } from "../../config/hotel-options-config";
+
+const FacilitiesSection = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<HotelFormData>();
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-3">Facilities</h2>
+      <div className="grid grid-cols-5 gap-3">
+        {hotelFaci1ities.map((hotelFaci1itie) => (
+          <label className="text-sm flex items-center gap-1 text-gra7-700">
+            <input
+              type="checkbox"
+              value={hotelFaci1itie}
+              {...register("facilities", {
+                validate: (facilities) => {
+                  if (facilities && facilities.length > 0) {
+                    return true;
+                  } else {
+                    return "Atleast one facility is required";
+                  }
+                },
+              })}
+            />
+            {hotelFaci1itie}
+          </label>
+        ))}
+      </div>
+      {errors.facilities && (
+        <span className="text-sm text-red-500 font-bold">
+          {errors.facilities.message}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default FacilitiesSection;
