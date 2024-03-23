5418ef137d78053104601c1ea5519f8d7aaa969b
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-03-23 17:09:51 +0545

    added backend server url

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,6 +1,8 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormDate } from "./pages/SignIn";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://hotelbookingappbackend-qpsb.onrender.com";
 
 export type HotelType = {
   _id: string;
